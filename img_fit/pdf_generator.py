822029a72953e648dff0b6b7eb31c33269e8810c
Vladimir <51376141+vchulikov@users.noreply.github.com>
2020-04-18 17:13:51 +0300

    Update pdf_generator.py

--- img_fit/pdf_generator.py
+++ img_fit/pdf_generator.py
@@ -1,18 +1,26 @@
 #generate hist follow pdf, save result in csv file
 import ROOT
 import numpy
+import math
 
 #create file, hist, array
 f = open("output_file.csv", "w")
-hist1 = ROOT.TH1F("h1", "h1", 30, 2250, 2320)
+hist1 = ROOT.TH1F("h1", "h1", 100, 2250, 2320)
 array = ([])
 
-#fill hist follow distribution
-for i in range(100000):
- val = ROOT.gRandom.Gaus(2290., 3.6)
- hist1.Fill(val)
+#CB
+#func = ROOT.TF1("fun","ROOT::Math::crystalball_function(x, 1., 4., 3.5, 2290)",2250,2320)
 
-#normalisation
+#GAUSSIAN
+mu = ROOT.TFormula("mu",str(2290))
+sigma = ROOT.TFormula("sigma",str(3.6))
+pi = math.pi
+func = ROOT.TF1("fun","[0]*exp((-0.5)*(x-mu)**2/(sigma**2))/(sigma**2*2*pi)**0.5",2250,2320)
+func.SetParameters(10,4,1,20)
+
+#fill hist follow distribution
+hist1.FillRandom("fun",20000);
+
 hist1.Scale(1./hist1.GetSumOfWeights())
 
 #fill array
@@ -22,8 +30,5 @@ for i in range(hist1.GetNbinsX()):
  array.append([bin_num, bin_cont])
  f.write("{0},{1}{2}".format(bin_num, bin_cont, "\n"))
 
-#fit
-hist1.Fit("gaus")
- 
 #print(array)
 hist1.Draw("e1")
