cdf559578671dab2f463b5508c1eebfee0dfd926
Vladimir <51376141+vchulikov@users.noreply.github.com>
2020-09-21 20:54:02 +0300

    Create 0_2_ascii_to_root.py

---
+++ Convert/0_2_ascii_to_root.py
@@ -0,0 +1,41 @@
+#SECOND PART
+#ASCII TO ROOT
+#REQUIREMENTS: ASCII-FILE (Xic_2012_8Tev_MagUp_ds_ksi2470.txt)
+
+import re, array, os
+import ROOT
+from ROOT import TFile, TTree, gROOT, addressof
+
+arr = ["pt","y","ct","ch","id","ipc","p_p","eta_p","phi_p","p_k","eta_k","p_3","eta_3","ntrk","NNp","im","m12","m13","m23","NNk","NN3","lgi"]
+
+
+gROOT.ProcessLine(
+"struct staff_t {\
+   Float_t pt;\
+   Float_t y;\
+   Float_t im;\
+   Float_t ntrk;\
+   Float_t lgi;\
+};");
+
+staff = ROOT.staff_t()
+
+f = TFile( 'Xic_2012_8Tev_MagUp.root', 'RECREATE' )
+tree = TTree( 'ds_ksi2470', 'staff data from ascii file' )
+tree.Branch( 'pt', ROOT.AddressOf( staff, 'pt' ), 'pt/F' )
+tree.Branch( 'y', ROOT.AddressOf( staff, 'y' ), 'y/F' )
+tree.Branch( 'im', ROOT.AddressOf( staff, 'im' ), 'im/F' )
+tree.Branch( 'ntrk', ROOT.AddressOf( staff, 'ntrk' ), 'ntrk/F' )
+tree.Branch( 'lgi', ROOT.AddressOf( staff, 'lgi' ), 'lgi/F' )
+
+for line in open("PATH-TO-DIR/Xic_2012_8Tev_MagUp_ds_ksi2470.txt").readlines():
+ t = list(filter( lambda x: x, re.split( '\s+', line ) ) )
+ staff.pt  = float(t[0])
+ staff.y  = float(t[1])
+ staff.im  = float(t[15])
+ staff.ntrk  = float(t[13])
+ staff.lgi  = float(t[21])
+ tree.Fill()
+
+tree.Print()
+tree.Write()
