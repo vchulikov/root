af18c26601c2e7883719b5bc838319d1250ec0f9
Vladimir <51376141+vchulikov@users.noreply.github.com>
2020-10-05 12:43:20 +0300

    Update csv_generator.py

--- ml_dist/csv_generator.py
+++ ml_dist/csv_generator.py
@@ -1,30 +1,38 @@
-#create one csv
+#create one csv by list of pdfs
 import ROOT
 import numpy
 import math
 
-f = open("./files/all_data.csv", "w")
-files_num = 200 #i've add 100 more events from desease file
+f = open( "./files/all_data.csv", "w")
+files_num = 200
 
+#####################################################################
+f.write("20.")
+f.write(",")
+f.write("100.")
+f.write(",")
 
-#open file, read data column and return this array
-def fill_arr(item):
- data = numpy.genfromtxt("./files/gen_file_"+str(item)+".csv", delimiter = ',')
- arr = []
- for i in range(len(data)):
-  arr.append(data[i, 1])
- return arr
+for i in range(100):
+    f.write("bin_" + str(i+1))
+    f.write(",")
 
-#create array to save
-a = []
+f.write("\n")
 
-for i in range(1, files_num+1):
- a.append(fill_arr(i))
+#####################################################################
 
+def write_file(item):
+    bins_num = 100
+    data = numpy.genfromtxt("./files/gen_file_"+ str(item) + ".csv", delimiter = ',')
 
-#write this array to file
-for i in range(101): #number of bins in hist
- for j in range(1, files_num+1):
-  f.write(str(a[j-1][i]))
-  f.write(",")
- f.write("\n")
+    for i in range(len(data)):
+        if i == bins_num:
+            f.write(str( int(data[i][1]) ))
+            f.write("\n")
+        else:
+            f.write(str(data[i][1]))
+            f.write(",")
+
+#####################################################################
+
+for i in range(files_num):
+    write_file(i+1)
