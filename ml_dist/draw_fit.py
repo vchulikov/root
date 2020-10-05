1e453679b1a398f691a76cb1a6020fcc68d73edb
Vladimir <51376141+vchulikov@users.noreply.github.com>
2020-10-05 12:45:05 +0300

    Update draw_fit.py

--- ml_dist/draw_fit.py
+++ ml_dist/draw_fit.py
@@ -1,10 +1,9 @@
-#from ROOT import *
 import ROOT
 import math
 import Imports
 
 #Draw picture
-img = ROOT.TImage.Open("./data_infarct.png")
+img = ROOT.TImage.Open("./data_1.png")
 canvas = ROOT.TCanvas("canvas1")
 img.Draw("x")
 
@@ -18,7 +17,11 @@ p.cd()
 #create func for inf
 #hist1 = Imports.two_gaussians(0., 250., 86., 33.5, 190., 33.5,"" , 4)
 #create func for norm
-hist1 = Imports.gaussian(0., 250., 128., 21, "" , 4)
+hist1 = Imports.gaussian(-4, 4., 0.3, 0.34, "" , 4)
+
+#sig: (-4, 4., 0.3, 0.34, "" , 4)
+#bkg: (-4, 4., 1.5, 0.8, "" , 5)
+
 
 #Set Frame-parameters
 p.SetMargin(0.15, 0.06, 0.19, 0.08)
